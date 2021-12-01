06f935ab3129a69195ee5c6f4711efeadcfc599d
Bharat <bchotwani3@gmail.com>
2021-12-01 10:13:44 +0530

    fix error

--- clock.js
+++ clock.js
@@ -11,10 +11,22 @@ function updateClock(){
         sec = now.getSeconds(),
         per = "AM";
 
+        if(hour == 0){
+            hour = 12;
+        } if(hour > 12){
+            hour = hour - 12;
+            per = "PM" ;
+        }
+
+        Number.prototype.pad = function(digits){
+            for(var n = this.toString(); n.length < digits; n = 0 + n);
+            return n;
+        }
+
         var months = ["January", "February", "March", "Aprill", "May", "June", "July", "August", "September", "October", "November", "December"];
         var weeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var ids = ["datetime","date","dayname","month","daynum","year","time","hour","mint","period"];
-        var values = [weeks[dateName],months[mo],dnum,yr,hour,mint,sec,per];
+        var ids = ["dayname","month","daynum","year","hour","mint","secnd","period"];
+        var values = [weeks[dateName],months[mo],dnum.pad(2),yr,hour.pad(2),mint.pad(2),sec.pad(2),per];
 
     for(let i=0; i<ids.length; i++){
     document.getElementById(ids[i]).firstChild.nodeValue = values[i] ;
