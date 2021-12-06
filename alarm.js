dfc7bc41fef61aee25f21d914b708cde792046d5
Bharat <bchotwani3@gmail.com>
2021-12-06 12:18:22 +0530

    refactor

--- alarm.js
+++ alarm.js
@@ -1,12 +1,23 @@
 const setAlarm = document.getElementById("setAlarm");
 
+var soundObj = new Audio("https://www.freespecialeffects.co.uk/soundfx/animals/duck1.wav") 
+
+function alarmBell(){
+  soundObj.play();
+}
+
 function startAlarm(){
-    var userValue = alarm.value ;
-    console.log(userValue);
-    const now = new Date(userValue);
-    console.log(now);
-    var diffMS = userValue - now ;
+    var alarm = document.getElementById('alarm');
+    var userValue = alarm.value;
+    alarmDate = new Date(userValue);
+    now = new Date()
+    let diffMS = alarmDate - now ;
     console.log(diffMS);
+    if(diffMS >= 0){
+        setTimeout(() => {
+            alarmBell()
+        }, diffMS);
+    }
 }
 
 setAlarm.addEventListener("click", startAlarm );
